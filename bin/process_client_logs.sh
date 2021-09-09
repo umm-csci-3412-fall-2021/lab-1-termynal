7e3278b1432b9d8b05d8e8ba1d2eeb708693fbf6
Biruk Mengistu <mengi024@bebop.morris.umn.edu>
2021-09-09 11:22:38 -0500

    finished implementing process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 DIRECTORY=$1
 
-cd "$DIRECTORY"
+cd "$DIRECTORY" || exit
 
-cat * > combined.txt
-awk 'match($0, /([a-zA-z]{3} [0-9]{2}) ([0-9]{2})[0-9:]+ ([a-zA-z]+).+ Failed password .+ ([a-zA-z ]+) from ([0-9.]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' combined.txt > failed_login_data.txt
+cat var/log/* > combined.txt
+awk 'match($0, /([a-zA-z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4] "\n" }' combined.txt > failed_login_data.txt
 
 
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -5,7 +5,7 @@ do
 	CLIENTNAME=$(echo "$var" |sed 's/([a-zA-z]{1,})_secure.tgz/\1/')
 	mkdir "$TEMPFOLDER/$CLIENTNAME"
 	tar -xzf "../log_files/$var" -C "../bin/$TEMPFOLDER/$CLIENTNAME"
-	./process_client_logs.sh
+	./process_client_logs.sh "$TEMPFOLDER/$CLIENTNAME"
 done
 
 ./create_username_dist.sh
