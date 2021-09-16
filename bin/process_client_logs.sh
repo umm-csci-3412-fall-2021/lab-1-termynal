2016865e7674afaa9a656eef9cbd1c7a5de0025f
mengi024 <mengi024@morris.umn.edu>
2021-09-16 11:11:56 -0500

    trying new implementation of process_client_logs

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,6 +5,6 @@ DIRECTORY=$1
 cd "$DIRECTORY" || exit
 
 cat var/log/* > combined.txt
-awk 'match($0, /([a-zA-z]{3} [0-9 ]{2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4] "\n" }' combined.txt > failed_login_data.txt
+awk 'match($0, /([a-zA-z]{3})  ?([0-9]{1,2}) ([0-9]{2})[0-9:]+ .+ Failed password .+ ([a-zA-z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]  " " groups[5] "\n" }' combined.txt > failed_login_data.txt
 
 
