0b014186a73b56eeb5cdb73b0d61e3c35b7eb2f2
mengi024 <mengi024@morris.umn.edu>
2021-09-16 10:24:43 -0500

    fixed bugs in create_username_dist

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,22 +4,13 @@ dirName=$1
 cd "$dirName" || exit
 
 combined=$(mktemp)
-find . -type f -name 'failed_login_data.txt' -exec cat {} \; > "$combined"
+find . -type f -name 'failed_login_data.txt' -exec cat {} + > "$combined"
 
 counts=$(mktemp)
-awk 'match($0, /.{1,} ([a-zA-z0-9]{1,}) [0-9.]+/, groups) {print groups[1] "\n" }' "$combined" | uniq -c > "$counts"
+awk 'match($0, /.{1,} ([a-zA-z0-9]{1,}) [0-9.]+/, groups) {print groups[1]}' "$combined" | sort | uniq -c > "$counts"
 
 body=$(mktemp)
-awk 'match($0, /([0-9]+) ([a-zA-Z0-9]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] ");" "\n" }' "$counts" > "$body"
+awk 'match($0, /([0-9]+) ([a-zA-Z0-9]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);" }' "$counts" > "$body"
 
-cd ..
-bin/wrap_contents.sh "$body" "username_dist" "username_dist.html"
-
-#dirs=("$(find . -type d)")
-#for dir in "${dirs[@]}"; do
-#  awk 'match($0, /.{1,} ([a-zA-z0-9]{1,}) [0-9.]+/, groups) {print groups[1] "\n" }' "$dir/failed_login_data.txt" | uniq -c
-#done
-
-
-
-
+cd - || exit
+./bin/wrap_contents.sh "$body" html_components/username_dist data/username_dist.html
