102d1836c914ec11e9c56750bc0a7a523afa1fe2
Biruk <mengi024@morris.umn.edu>
2021-09-22 14:10:22 -0500

    Add explanatory comments

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -3,12 +3,14 @@ dirName=$1
 
 cd "$dirName" || exit
 
+# Combine all failed_login_data.txt files from each folder into one file.
 combined=$(mktemp)
 find . -type f -name 'failed_login_data.txt' -exec cat {} + > "$combined"
 
 counts=$(mktemp)
 awk 'match($0, /[a-zA-Z]{3} [0-9]{1,2} ([0-9]{1,2}) .+/, groups) {print groups[1]}' "$combined" | sort | uniq -c > "$counts"
 
+# Generate the "body" of the output file, meaning the part that sits between the header and footer.
 body=$(mktemp)
 awk 'match($0, /([0-9]+) ([0-9]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);" }' "$counts" > "$body"
 
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
 TEMPFOLDER=$(mktemp -d "temp.XXXXXXXXXXXXXXX")
 
+# Iterate through each file argument passed
 for var in "$@"
 do
+  # Extract the file name without the extension
 	CLIENTNAME=$(echo "$var" | awk 'match($0, /([a-zA-z]{1,}).tgz/, groups) {print groups[1]}')
 	mkdir "$TEMPFOLDER/$CLIENTNAME"
 	tar -xzf "$var" -C "$TEMPFOLDER/$CLIENTNAME"
